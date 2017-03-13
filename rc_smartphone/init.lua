2dde46f8e664ed05ff1e1804e84b6e530a752474
cpdef <cpdef@protonmail.com>
2017-03-13 22:45:32 +0100

    add the smartphone

---
+++ rc_smartphone/init.lua
@@ -0,0 +1,9 @@
+minetest.register_tool("rc_smartphone:smartphone", {
+                description = "Smartphone",
+                inventory_image = "smartphone.png",
+	        stack_max = 1,
+                on_use = function(itemstack, player, pointed_thing)
+		        pos = pointed_thing.under
+		        minetest.chat_send_player(player:get_player_name(), '{x='..pos.x..',y='..pos.y..',z='..pos.z..'}')
+	        end,
+		})
